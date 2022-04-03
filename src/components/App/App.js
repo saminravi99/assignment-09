e25fbe05c3cb48ba06a3dcf70e97949bbdf069ea
Samin Israr Ravi <saminisrar1@gmail.com>
2022-04-03 20:50:47 +0600

    project initialized

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+  <React.StrictMode>
+    
+  </React.StrictMode>
+  );
+}
+
+export default App;
