3aaa2371bce82644e9294717b453ba832f0624d9
Samin Israr Ravi <saminisrar1@gmail.com>
2022-04-04 01:46:05 +0600

    review section added

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AllContext } from '../App/App';
+import EachReview from './EachReview/EachReview';
 import './Reviews.css';
 
+
+
 const Reviews = () => {
+
+    const {reviews} = useContext(AllContext);
+
+    const review = reviews.map(review => {
+        return(
+            <EachReview
+                key={review.id}
+                {...review}
+            ></EachReview>
+        )
+    })
+
     return (
-        <div>
-            <h1>review</h1>
+        <div className="row container mx-auto my-4">
+            {review}
         </div>
     );
 };
