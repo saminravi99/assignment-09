677947af8cea8b2caab9b7903e040a5581bfddac
Samin Israr Ravi <saminisrar1@gmail.com>
2022-04-04 01:15:23 +0600

    Home Page Completed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,11 @@
 import React, { useContext }  from 'react';
 import { useNavigate } from 'react-router-dom';
-import HeroImage from "../../img/hero-img.png"
 import { AllContext } from '../App/App';
+import HeroSection from '../HeroSection/HeroSection';
 import "./Home.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar, faStarHalfStroke} from '@fortawesome/free-solid-svg-icons'
+import { StarIcon } from '@heroicons/react/outline'
 
 const Home = () => {
 
@@ -19,8 +22,44 @@ const Home = () => {
                 <div className="card home-review-container  h-100">
                     <div className="card-body">
                         <cite className="card-text home-review">"{review.review}"</cite>
-                        <h6 className="card-title home-review-name mt-4">{review.name},</h6>
-                        <p className="card-text home-review-date">{review.date}</p>
+
+                        
+
+                        <h6 className="pt-4 card-text home-review-rating">Ratings:
+
+                       <span className="ms-2">
+                            {review.rating >= 4.5 ? 
+                            <span>
+                                <FontAwesomeIcon className="home-review-star" icon={faStar} />
+                                <FontAwesomeIcon className="home-review-star" icon={faStar} />
+                                <FontAwesomeIcon className="home-review-star" icon={faStar} />
+                                <FontAwesomeIcon className="home-review-star" icon={faStar} />
+                                <FontAwesomeIcon className="home-review-star" icon={faStarHalfStroke} />
+                            </span>
+                            :
+                            <span>
+                                
+                                <FontAwesomeIcon className="home-review-star" icon={faStar} />
+                                <FontAwesomeIcon className="home-review-star" icon={faStar} />
+                                <FontAwesomeIcon className="home-review-star" icon={faStar} />
+                                <FontAwesomeIcon className="home-review-star" icon={faStar} />
+                                <StarIcon className="star-icon"></StarIcon>
+                            </span>
+                        }
+                       </span>
+
+                        <span className="ms-2">
+                            ({review.rating})
+                        </span>
+                        </h6>
+
+                        <h6 className="card-title home-review-name mt-4">
+                            <span>
+                                <img className="home-review-image" src={review.image} alt="img" />
+                            </span>
+                            {review.name},</h6>
+
+                        <p className="card-text home-review-date mt-3">{review.date}</p>
                     </div>
                 </div>
             </div>
@@ -29,37 +68,9 @@ const Home = () => {
 
     return (
         <div>
-            <div className="container row my-5 mx-auto d-flex flex-lg-row flex-column-reverse">
-                <div className="col-lg-8 col-12 d-flex flex-lg-row flex-column-reverse">
-                    <div className="d-flex  align-items-center h-100">
-                        <div className="w-75 hero-container">
-                            <h1 className=" hero-text">
-                            Order food from the best restaurant near you.
-                            </h1>
+            {/* Hero Section   */}
 
-                            <p className="mt-4 hero-detail">
-                                Order food from your favourite restaurants, delivered straight to your front door. Browse restaurants near you and pick from their menus. Get a better idea of what's on offer before you order by checking out full-screen photos of food. Wherever you are in the world, there's a restaurant for you.  
-                            </p>
-
-                            <div>
-                                <div>
-                                    <button onClick={()=> navigate("/about")} className="btn hero-btn mt-2">
-                                    Explore
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    
-                </div>
-                <div className="col-lg-4 col-12">
-                    <div className="d-flex  justify-content-center align-items-center ">
-                        <div className="h-100">
-                            <img className="hero-img" src={HeroImage} alt="img" />
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <HeroSection></HeroSection>
 
             {/* Review Section     */}
 
